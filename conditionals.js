de40f787519cb2dceffe3ac49dbd315c4b5a4ee2
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-06-08 22:08:51 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,5 @@
 const temperature = -2;
+var raining = true;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -6,6 +7,14 @@ if (temperature < 0) {
   console.log("Short sleeves won't cut it!");
 } else {
   console.log("Short sleeves are fine.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
 }
 
 console.log("Now you're ready to go outside!");
